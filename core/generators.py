7ac2e6276c2c3a1fe2f896f99d627e7b9b72a6c3
Vlad Kobzar <kobzar_vladislav@mail.ru>
2020-01-20 01:12:00 +0300

    Generators

--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,9 @@
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
+from random import randrange
 def gen(N):
-    pass
+    for i in range(N):
+        yield randrange(1, 100)
 
+N = 10
 # написать генераторное выражение, которое делает то же самое
+(randrange(1, 100) for i in range(N))
